fd0a17c990a3cbc1d6caa913123510b54081b476
gagan <gagandeep.khaira@somrubioscience.com>
2025-05-21 11:15:35 -0300

    feat(nav-bar): added nav bar componet to shuttle dashboard

---
+++ shuttle/src/app/Components/shuttle-dashboard/shuttle-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-shuttle-dashboard',
+  imports: [NavBarComponent],
+  templateUrl: './shuttle-dashboard.component.html',
+  styleUrl: './shuttle-dashboard.component.scss'
+})
+export class ShuttleDashboardComponent {
+
+}
