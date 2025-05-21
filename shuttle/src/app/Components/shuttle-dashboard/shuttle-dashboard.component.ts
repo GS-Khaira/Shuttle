7ed81f51504e075864ae2a8c7c6323afb76d10a0
gagan <gagandeep.khaira@somrubioscience.com>
2025-05-21 11:55:54 -0300

    feat(home): created home page and adjusted sop dashboard css

--- shuttle/src/app/Components/shuttle-dashboard/shuttle-dashboard.component.ts
+++ shuttle/src/app/Components/shuttle-dashboard/shuttle-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { HomeComponent } from "../home/home.component";
 
 @Component({
   selector: 'app-shuttle-dashboard',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, HomeComponent],
   templateUrl: './shuttle-dashboard.component.html',
   styleUrl: './shuttle-dashboard.component.scss'
 })
