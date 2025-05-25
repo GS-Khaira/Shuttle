f33d4cf0d1f75f1d6cff68dac4b75ee2442b5e16
gagan <gagandeep.khaira@somrubioscience.com>
2025-05-25 15:05:24 -0300

    feat(Post-ride): add post ride component

---
+++ shuttle/src/app/Components/post-ride/post-ride.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-post-ride',
+    templateUrl: './post-ride.component.html',
+    styleUrls: ['./post-ride.component.scss']
+})
+export class PostRideComponent{
+
+}
