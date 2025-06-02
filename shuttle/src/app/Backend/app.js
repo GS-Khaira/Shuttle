b5f1d8f65c733a984addd0bb4e72cb1bcaaf6dd2
gagan <gagandeep.khaira@somrubioscience.com>
2025-06-02 14:15:56 -0300

    feat(sql-database): connected to sql database

--- shuttle/src/app/Backend/app.js
+++ shuttle/src/app/Backend/app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const db = require('./util/database');
 
 app.use(cors());
 app.use(express.json());
+
+db.execute('Select * FROM users')
+.then(result => {
+    console.log(result);
+})
+.catch(
+    err =>{
+        console.log(err);
+    }
+);
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
