efcc52d54f32f4286199ed4ff6b477f433057742
gagan <gagandeep.khaira@somrubioscience.com>
2025-06-10 12:22:39 -0300

    feat(signIn): sign in page layout made

--- shuttle/src/app/app.routes.ts
+++ shuttle/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { PostRideComponent } from './Components/post-ride/post-ride.component';
 import { FindShuttleComponent } from './Components/find-shuttle/find-shuttle.component';
+import { SignInComponent } from './Components/sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'post-ride', component: PostRideComponent },
     { path: 'find-shuttle', component: FindShuttleComponent },
+    { path: 'signIn', component: SignInComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
