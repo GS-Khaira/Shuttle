979c5479fa894b3a6c3cd17e84b03ba1754748bc
gagan <gagandeep.khaira@somrubioscience.com>
2025-06-10 11:13:09 -0300

    feat(sequilize): sequlize utilized to create a new table user

--- shuttle/src/app/Backend/app.js
+++ shuttle/src/app/Backend/app.js
@@ -2,25 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const User = require('./models/user');
 
 app.use(cors());
 app.use(express.json());
 
-db.execute('Select * FROM users')
-.then(result => {
-    console.log(result);
-})
-.catch(
-    err =>{
-        console.log(err);
-    }
-);
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connection established successfully.');
+        return sequelize.sync();
+    })
+    .then(() => {
+        console.log('Database synced successfully');
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:3000`);
+        });
+    })
+    .catch(err => {
+        console.error('Database error:', err);
+    });
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
 });
