ae5fcff19eac2583804066d8af2fb7080c8204ca
gagan <gagandeep.khaira@somrubioscience.com>
2025-06-11 11:58:52 -0300

    feat(signUp): sign up component in routes

--- shuttle/src/app/app.routes.ts
+++ shuttle/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './Components/home/home.component';
 import { PostRideComponent } from './Components/post-ride/post-ride.component';
 import { FindShuttleComponent } from './Components/find-shuttle/find-shuttle.component';
 import { SignInComponent } from './Components/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'post-ride', component: PostRideComponent },
     { path: 'find-shuttle', component: FindShuttleComponent },
     { path: 'signIn', component: SignInComponent},
+    { path: 'signUp', component: SignUpComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
