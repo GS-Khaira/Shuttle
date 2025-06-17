a6d9e895f2a2affdd5ab08ab8284a78171521906
gagan <gagandeep.khaira@somrubioscience.com>
2025-06-17 13:03:38 -0300

    feat(post ride): suggested dates are fetch through google docs api

--- shuttle/src/app/Components/post-ride/post-ride.component.ts
+++ shuttle/src/app/Components/post-ride/post-ride.component.ts
@@ -1,15 +1,19 @@
-import { Component} from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RideService } from '../../Services/ride.service';
+import { environment } from '../../../environment/environment';
+
 
 @Component({
     selector: 'app-post-ride',
     templateUrl: './post-ride.component.html',
     imports: [FormsModule],
-    styleUrls: ['./post-ride.component.scss']
+    styleUrls: ['./post-ride.component.scss'],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PostRideComponent{
-     ride = {
+  apiKey = environment.googleMapsApiKey;  
+  ride = {
         from: '',
         to: '',
         date: '',
@@ -21,9 +25,25 @@ export class PostRideComponent{
         comments: ''
   };
 
+  @ViewChild('placePicker') placePickerRef!: ElementRef;
+  @ViewChild('placeDropper') placeDropperRef!: ElementRef;
+
   constructor(private rideService: RideService) {}
 
+  ngAfterViewInit(): void {
+  const picker = this.placePickerRef.nativeElement as any;
+  const dropper = this.placeDropperRef.nativeElement as any;
+
+  picker.addEventListener('gmpx-placechange', (event: any) => {
+    this.ride.from = (event.target as any).value.formattedAddress;
+  });
+  dropper.addEventListener('gmpx-placechange', (event: any) => {
+    this.ride.to = (event.target as any).value.formattedAddress;
+  });
+}
+
   onSubmit() {
+    console.log(this.ride);
     this.rideService.postRide(this.ride).subscribe({
       next: res => {
         console.log('Ride posted:', res);
---
+++ shuttle/src/environment/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  googleMapsApiKey: 'AIzaSyAeIdM8HTuf5qxWR0NWyyaeiLp4mAVCsBk'
+};
